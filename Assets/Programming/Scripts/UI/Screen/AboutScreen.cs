68a3a8dc47065b5b360f4669d3785d4bb689b18d
Louis GARRIGUES <l.garrigues@student.isartdigital.com>
2024-04-25 15:29:50 +0200

    Update screens animations

--- Assets/Programming/Scripts/UI/Screen/AboutScreen.cs
+++ Assets/Programming/Scripts/UI/Screen/AboutScreen.cs
@@ -1,8 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMFunds.UI;
 using UnityEngine;
 
 public class AboutScreen : ScreenUI
 {
+    public static Action OnBack;
 
+    [SerializeField] private AnimatedButton _back;
+
+    override protected void Start()
+    {
+        _back.OnPlay += Back;
+    }
+
+    private void Back()
+    {
+        OnBack?.Invoke();
+    }
 }
