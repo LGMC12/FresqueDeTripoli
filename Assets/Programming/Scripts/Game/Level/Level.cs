4371df5998ddeb9d08c9a82548615807d4db3ae0
LouisG <louis.garrigues.lg12@gmail.com>
2024-07-29 16:37:39 +0200

    add level phases

--- Assets/Programming/Scripts/Game/Level/Level.cs
+++ Assets/Programming/Scripts/Game/Level/Level.cs
@@ -1,122 +1,172 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+
+public enum EGamePhase
+{
+	Seeds, 
+	Cheeses, 
+	Adds,
+}
 
 public class Level : MonoBehaviour
 {
 	public static Action<Vector3> InitPlayerPosition;
+	public static Action OnLevelClear;
+	public static Action OnSeedPhaseComplete;
 
-	private static List<Level> _levels = new List<Level>();
+	protected EGamePhase m_phase = EGamePhase.Seeds;
 
-	private static int _currentLevelID = 0;
-
-	public static Level CurrentLevel
-	{ get => _levels[_currentLevelID]; }
-
-	[SerializeField] private int levelID;
-	private bool isOpen = false;
 	[Header("Harvestables")]
-	[SerializeField] private GameObject _seedsOpen;
-	[SerializeField] private GameObject _seedsClose;
-	[SerializeField] private GameObject _cheesesOpen;
-	[SerializeField] private GameObject _cheesesClose;
-	[SerializeField] private GameObject _flowers;
+	[SerializeField] protected GameObject m_seeds;
+	[SerializeField] protected GameObject m_cheeses;
+	[SerializeField] protected GameObject m_adds;
+	[SerializeField] protected GameObject m_flowers;
+
 	[Header("Shops")]
 	[SerializeField] private GameObject _startShop;
 	[SerializeField] private List<GameObject> _shops = new List<GameObject>();
-	[Header("Obstacles")]
-	[SerializeField] private List<GameObject> _doors = new List<GameObject>();
+
+	[Header("Doors")]
+	[SerializeField] protected List<GameObject> m_doorsList = new List<GameObject>();
+
 	[Header("Background")]
-	[SerializeField] private SpriteRenderer _openBG;
-	[SerializeField] private SpriteRenderer _closeBG;
+	[SerializeField] protected SpriteRenderer m_baseBG;
+	[SerializeField] protected SpriteRenderer m_openBG;
+	[SerializeField] protected SpriteRenderer m_negatedBG;
+
 	[Header("Enemies")]
-	[SerializeField] private List<Enemy> _enemies = new List<Enemy>();
+	[SerializeField] protected GameObject m_enemies; 
 
+	[Header("Pixels Zones")]
+	[SerializeField] protected GameObject m_zones;
+	protected List<Zone> m_zonesList = new List<Zone>();
+	public List<Zone> ZonesList { get { return m_zonesList; } }
 
-	private void Awake()
+	[Header("Traps")]
+	[SerializeField] protected GameObject m_hiddenTraps;
+	[SerializeField] protected GameObject m_hiddableTraps;
+
+	protected List<HarvestableObject> m_toDestroy = new List<HarvestableObject>();
+
+	public Vector3 RespawnPoint {  get; protected set; }
+	protected virtual void Awake()
 	{
-		HarvestableObject.OnHarvested += HarvestableObject_OnHarvested_Close;
+		HarvestableObject.OnHarvested += HarvestableObject_OnHarvested;
+		Player.OnDeath += Player_OnDeath;
 
-		_levels.Add(this);
+		m_zonesList = m_zones.GetComponentsInChildren<Zone>().ToList();
 	}
 
-	private void Start()
+	private void OnEnable()
 	{
-		InitPlayerPosition?.Invoke(_startShop.transform.position);
+		m_totalSeeds = m_seedsCount = m_seeds.transform.childCount;
+		m_totalCheeses = m_cheesesCount = m_cheeses.transform.childCount;
+		m_totalAdds = m_addsCount = m_adds.transform.childCount;
 
-		_seedsOpen.SetActive(false);
-		_cheesesOpen.SetActive(false);
+		InitPlayerPosition?.Invoke(m_startShop.transform.position);
+
+		m_enemies.SetActive(true);
 	}
 
-	public void	CheckOpening()
+	private void Player_OnDeath()
 	{
-		if (_seedsClose.transform.childCount == 0 && _cheesesClose.transform.childCount == 0) OpenLevel();
-	}
+		foreach (HarvestableObject pHarvested in m_toDestroy)
+		{
+			pHarvested.Respawn();
+		}
 
-	private void HarvestableObject_OnHarvested_Close(HarvestableObject pHarvested)
+		m_toDestroy.Clear();
+
+		m_seedsCount = m_seeds.transform.childCount; 
+		m_cheesesCount = m_cheeses.transform.childCount;
+		m_addsCount = m_adds.transform.childCount;
+    }
+
+	protected virtual void HarvestableObject_OnHarvested(HarvestableObject pHarvested)
 	{
+		m_toDestroy.Add(pHarvested);
+
 		switch (pHarvested.HarvestableType)
 		{
 			case E_HarvestableType.Seed:
-
-				break;
-
+				--m_seedsCount;
+					break;
 			case E_HarvestableType.Cheese:
-
-				break;
-
+				--m_cheesesCount;
+                break;
+			case E_HarvestableType.Adds:
+				--m_addsCount;
+                break;
 			default:
 				break;
 		}
 
-		DestroyImmediate(pHarvested);
 	}
 
-	private void HarvestableObject_OnHarvested_Open(HarvestableObject pHarvested)
 	{
-		switch (pHarvested.HarvestableType)
+
+
+
+
+
+
+	public virtual void	CheckNextPhase()
+	{
+		switch (m_phase)
 		{
-			case E_HarvestableType.Seed:
-
-			break;
-
-			case E_HarvestableType.Cheese:
-
-			break;
-
+			case EGamePhase.Seeds:
+				if (m_seeds.transform.childCount == 0) CheesePhase();
+				break;
+			case EGamePhase.Cheeses:
+				if (m_cheeses.transform.childCount == 0) AddsPhase();
+				break;
+			case EGamePhase.Adds:
+				if (m_adds.transform.childCount == 0) LevelClear();
+				break;
 			default:
 				break;
 		}
-
-		DestroyImmediate(pHarvested);
-
-		if (_seedsOpen.transform.childCount == 0 && _cheesesOpen.transform.childCount == 0 && _flowers.transform.childCount == 0)
-		{
-			print("Level clear !");
-		}
 	}
 
-	private void OpenLevel()
+	protected virtual void CheesePhase()
 	{
-		print("Open doors");
-		HarvestableObject.OnHarvested += HarvestableObject_OnHarvested_Open;
-		HarvestableObject.OnHarvested -= HarvestableObject_OnHarvested_Close;
-
-		foreach (GameObject pDoor in _doors)
+		foreach (GameObject pDoor in m_doorsList)
 		{
 			pDoor.SetActive(false);
 		}
 
-		_closeBG.gameObject.SetActive(false);
-		_openBG.gameObject.SetActive(true);
+		m_baseBG.gameObject.SetActive(false);
+		m_openBG.gameObject.SetActive(true);
 
-		_seedsOpen.SetActive(true);
-		_cheesesOpen.SetActive(true);
+		m_phase = EGamePhase.Cheeses;
+
+		OnSeedPhaseComplete?.Invoke();
+
+    }
+
+	protected virtual void AddsPhase()
+	{
+		m_phase = EGamePhase.Adds;
+
+		m_openBG.gameObject.SetActive(false);
+		m_negatedBG.gameObject.SetActive(true);
+
+		m_hiddenTraps.gameObject.SetActive(true);
+		m_hiddableTraps.gameObject.SetActive(false);
+    }
+
+	protected virtual void LevelClear()
+	{
+		print("LEVEL CLEAR");
+
+		OnLevelClear?.Invoke();
 	}
 
-	private void OnDestroy()
+	protected virtual void OnDestroy()
 	{
-		HarvestableObject.OnHarvested -= HarvestableObject_OnHarvested_Open;
+		HarvestableObject.OnHarvested -= HarvestableObject_OnHarvested;
+		Player.OnDeath -= Player_OnDeath;
 	}
 }
